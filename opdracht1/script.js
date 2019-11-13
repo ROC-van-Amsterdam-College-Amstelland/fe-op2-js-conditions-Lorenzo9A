6e75e3c40f0d610c2e29147bc3806f5df5db6a5b
Lorenzo Breedveld <2105144@talnet.nl>
2019-11-13 11:44:52 +0100

    Opdracht 1

--- opdracht1/script.js
+++ opdracht1/script.js
@@ -1,25 +1,32 @@
 // A) maak 2 verschillende variabelen met twee willekeurige integers (zelf kiezen)
 
 // jouw code hierondeer-->> 
-var ;
-var ;
+var getal1 = 10;
+var getal2 = 20;
 
 // B) toon beide getallen op het scherm met document.getElementById('<jouw_gekozen_id>').innerHTML
 
 // jouw code hieronder-->>
-document.getElementById
+document.getElementById("n1").innerHTML = getal1;
+document.getElementById("n2").innerHTML = getal2;
 
 // C) Vraag de gebruiker om een INPUT waarde door prompt() te gebruiken. 
 // Gebruik de prompt()-waarde om een variabelen te 'vullen'
+var getal1 = prompt("Input")
+var getal2 = prompt("Input")                          
 
 // jouw code hieronder-->>
-var ;
-var ;
-
+// var ;
+// var ;
+ 
 // D) Vergelijk de twee getallen met elkaar door gebruik te maken van conditions  if()
 
 // jouw code hieronder-->>
-if () {
+if (getal1 < getal2) {
+    document.getElementById("main").style.backgroundColor = "yellow";
 
 }
+
+
+
 
