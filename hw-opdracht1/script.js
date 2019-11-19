197300fc1b67c09b008a9910c00bcb166cd4c054
Lorenzo Breedveld <2105144@talnet.nl>
2019-11-19 11:03:38 +0100

    hw1 opdracht

--- hw-opdracht1/script.js
+++ hw-opdracht1/script.js
@@ -1,14 +1,37 @@
-function Uitvoer() {
-    
-  }
+function bereken(){
+  var getal1;
+  var getal2;
 
-  var getal1 = 10;
-  var getal2 = 20;
 
+
+
+  getal1 = document.getElementById("invoerveld1").value;
+  getal2 = document.getElementById("invoerveld2").value;
 
   if(getal1 > getal2){
-      document.getElementById("Uitvoer").innerHTML;
+    
+    var totaal = Number(getal1) + Number(getal2);
+
+ }
+
+ if(getal1 <= getal2){
+    var totaal = Number(getal1) - Number(getal2);
 
 
+ }
 
-  }
+ if(getal1 ==10 &&  getal1 <= getal2){
+  var totaal = Number(getal1) + Number(getal2);
+
+
+ }
+
+ if(getal1 ==5 &&  getal1 >= getal2){
+  var totaal = Number(getal1) - Number(getal2);
+
+
+ }
+
+
+  alert(totaal);
+}
